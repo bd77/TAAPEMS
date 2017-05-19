6b05613e38331f0c195143c49e32fe8ca416b228
bartdegraeuwe <bart.degraeuwe@jrc.europa.eu>
2017-05-19 17:54:27 +0200

    All 3 formats of T-hot NEDC tests are now imported correctly

--- read_TrackNEDCHot_Bart_20170517.R
+++ read_TrackNEDCHot_Bart_20170517.R
@@ -14,7 +14,6 @@ paths <- c('D:/cyclebeating/Analysis TAA data/UK PEMS/euro-5-vehicle-data/',
 Track.hotNEDC.results.1 <- data.frame()
 Track.hotNEDC.results.2 <- data.frame()
 Track.hotNEDC.results.3 <- data.frame()
-Track.hotNEDC.results.4 <- data.frame()
 
 overview.TrackHotNEDC <- data.frame(car.name = c(), header1 = as.character(), num.cols = as.integer())
 
@@ -44,10 +43,10 @@ for (path in paths) {
   }
 }
 overview.TrackHotNEDC.sorted <- overview.TrackHotNEDC[order(overview.TrackHotNEDC$header, overview.TrackHotNEDC$num.cols),]
-write.table(overview.TrackHotNEDC, 'overview_TrackHotNEDC.txt', row.names = FALSE, quote = FALSE)
+write.table(overview.TrackHotNEDC.sorted, 'overview_TrackHotNEDC.txt', row.names = FALSE, quote = FALSE, sep = ';')
 
-path <- paths[2]
-file <- "euro-6-ford-focus-1499.xlsx"
+path <- paths[1]
+file <- "euro-5-hyundai-i30automatic-1582.xlsx"
 
 # loop over all test and put the results in a data frame according to the format.
 # The following formats exist:
@@ -61,7 +60,6 @@ for (path in paths) {
   for (file in xlsx.files) {
     print(paste('processing', file))
     car.name <- sub(".xlsx", "", file)
-    overview.TrackHotNEDC <- rbind(overview.TrackHotNEDC, data.frame(car.name = car.name, TrackHotNEDC = -1))
     
     # get the sheet number of the 'Track Hot NEDC' test
     sheets <- excel_sheets(paste0(path, file))
@@ -82,7 +80,7 @@ for (path in paths) {
       # by retrieving the list of header names it's possible to check the kind of data
       header.names <- names(test.data)
 
-      if (header.names[1] == 'Absolute_time') {
+      if (header.names[3] == 'Absolute_time') {
         # 2 possibilities: 138 columns or 174 columns (=138 + the other format)
         # the plot is the same for both number of columns
         jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
@@ -102,20 +100,23 @@ for (path in paths) {
           print('Problem: other number of columns for format Absolute_time')
         }
         
-        # # add data to dataframe (tibble)
-        # test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
-        # if (NCOL(test.data) == 140) {
-        #   Track.hotNEDC.results.2 <- rbind(Track.hotNEDC.results.2, test.data)
-        #   overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 2
-        # } else if (NCOL(test.data) == 176) {
-        #   Track.hotNEDC.results.3 <- rbind(Track.hotNEDC.results.3, test.data)
-        #   overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 3
-        # } else {
-        #   overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- NCOL(test.data)
-        #   print(paste(car.name, 'has', NCOL(test.data), 'columns'))
-        # }
+      } else if (header.names[3] == 'Test.SequenceNumber') { # R replaces spaces by a point
         
-      } else if (header.names[1] == 'Test SequenceNumber') {
+        # There is a format with 36 and 52 columns. The extra columns contain OBD data but
+        # they're empty or constant. So the are just left out
+        # some plots to check the data
+        jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
+        par(mfrow=c(1,2))
+        plot(test.data$GPS.Speed, main = car.name, type = 'l') # on road NEDC
+        plot(test.data$GPS.Longitude, test.data$GPS.Latitude, type = 'l') # you can see the test track shape
+        dev.off()
+
+        # add data to dataframe (tibble)
+        # add a column with the car name and test type
+        test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
+        Track.hotNEDC.results.2 <- rbind(Track.hotNEDC.results.2, test.data[,1:36])
+        
+      } else if (header.names[3] == 'TestSequenceNumber') {
         
         # some plots to check the data
         jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
@@ -123,41 +124,19 @@ for (path in paths) {
         plot(test.data$GPSSpeed, main = car.name, type = 'l') # on road NEDC
         plot(test.data$GPSLongitude, test.data$GPSLatitude, type = 'l') # you can see the test track shape
         dev.off()
-        
+
         # add data to dataframe (tibble)
         # add a column with the car name and test type
         test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
-        Track.hotNEDC.results.1 <- rbind(Track.hotNEDC.results.1, test.data)
-        overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 1
-        
-      } # stop reading format 1
-      
-      # 2) almost the same as format 1, same number of columns (52) but other names (with spaces) and some different fields
-      else if (header.names[1] == 'Test SequenceNumber') {
-        # some plots to check the data
-        jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
-        par(mfrow=c(1,2))
-        plot(test.data$`GPS Speed`, main = car.name, type = 'l') # on road NEDC
-        plot(test.data$`GPS Longitude`, test.data$`GPS Latitude`, type = 'l') # you can see the test track shape
-        dev.off()
-        
-        # add data to dataframe (tibble)
-        # add a column with the car name and test type
-        test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
-        Track.hotNEDC.results.4 <- rbind(Track.hotNEDC.results.4, test.data)
+        Track.hotNEDC.results.3 <- rbind(Track.hotNEDC.results.3, test.data)
         overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 4
         
-      }
-      
-      else 
-      else {
+      } else {
         print('unknown header')
       }
     } # if there are data
   } # loop over files
 } # loop over folders
-
-print(overview.TrackHotNEDC)
 
 length(unique(Track.hotNEDC.results.1$car.name))
 length(unique(Track.hotNEDC.results.2$car.name))
